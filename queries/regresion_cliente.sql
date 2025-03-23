728b86eff96425efba3de8f0184cd47a5610cbe0
martinagg7 <martinagg@myuax.com>
2025-03-23 19:17:26 +0100

    prueba con regresion lineal

--- queries/regresion_cliente.sql
+++ queries/regresion_cliente.sql
@@ -1,15 +1,17 @@
--- regresion_cliente.sql:datos necesitamos para estimar la variable churn de un cliente
-
 SELECT
-  f.Customer_ID,
-  AVG(c.Edad) AS Edad_Media_Cliente,
-  AVG(f.Car_Age) AS Edad_Media_Coche,
-  AVG(f.Margen_Bruto) AS Margen_Bruto_Medio,
-  AVG(f.Margen_eur) AS Margen_Eur_Medio,
-  AVG(c.RENTA_MEDIA_ESTIMADA) AS Renta_Media,
-  AVG(CAST(f.Churn AS DECIMAL(10,2))) AS churn_medio -- Esto sería la variable objetivo
+    f.PVP,
+    COUNT(*) AS Numero_Compras,  -- Número de compras por precio (PVP)
+    AVG(c.Edad) AS Edad_Media_Cliente,
+    AVG(f.Car_Age) AS Edad_Media_Coche,
+    AVG(f.Margen_Bruto) AS Margen_Bruto_Medio,
+    AVG(f.Margen_eur) AS Margen_Eur_Medio,
+    AVG(c.RENTA_MEDIA_ESTIMADA) AS Renta_Media,
+    AVG(f.Km_medio_por_revision) AS Km_medio_por_revision,
+    AVG(f.DIAS_EN_TALLER) AS Dias_En_Taller,
+    AVG(f.Coste_Total) AS Coste_Total,
+    AVG(CAST(f.Churn AS DECIMAL(10,2))) AS churn_medio  -- Variable objetivo
 FROM [dwh_case1].[dbo].[dim_fact] f
-JOIN [dwh_case1].[dbo].[dim_cliente] c
-  ON f.Customer_ID = c.Customer_ID
-GROUP BY f.Customer_ID
-ORDER BY f.Customer_ID;
+JOIN [dwh_case1].[dbo].[dim_cliente] c 
+    ON f.Customer_ID = c.Customer_ID
+GROUP BY f.PVP
+ORDER BY f.PVP;
